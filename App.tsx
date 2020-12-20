781f3cb4c19bbfe6331f7084429dd19c7080f361
iris766 <iris.tech766@gmail.com>
2020-12-20 10:46:32 +0500

    Login screen added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {  StyleSheet, Text, TextInput, TouchableOpacity, View, Image, ScrollView } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This will be the conditional Login page.</Text>
-      <Text>This screen will only render when the user is
-        <Text style={styles.bold}> Register </Text> 
-        or loged in.</Text>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scroll}>
+        <StatusBar style="auto" backgroundColor={'#fff'} />
+        <Text style={styles.welcome}>Welcome to</Text>
+        <Text style={styles.heading2}>Sahib Real Estate and Builders</Text>
+        <Image source={require('./assets/login.png')} style={styles.img} />
+        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput placeholder="Password" style={styles.input} secureTextEntry={true} />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={{ color: '#fff', textAlign: 'center' }}>LOGIN</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
@@ -18,13 +24,44 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     textAlign: 'center',
-    padding: 10
   },
+  scroll: {
+    paddingHorizontal: 35,
+    paddingTop: 30,
+  },
+
   bold: {
     color: '#333',
     fontWeight: 'bold',
+  },
+
+  welcome: {
+    fontSize: 32,
+    color: '#525F82',
+  },
+  heading2: {
+    fontSize: 42,
+    color: '#525F82',
+    fontWeight: 'bold',
+  },
+  input: {
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 4,
+    paddingHorizontal: 10,
+    borderColor: '#ddd',
+    borderRadius: 16,
+  },
+  btn: {
+    backgroundColor: '#097FA4',
+    width: 100,
+    paddingVertical: 8,
+    borderRadius: 18,
+    alignSelf: 'center',
+  },
+  img: {
+    alignSelf: 'center',
+    height: 300
   }
 });
---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Hello This is login screen</Text>
+    </View>
+  )
+}
